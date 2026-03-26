b3050c582adb4db43cd970c022f6991b48c22a56
enseor <enriqueseor@gmail.com>
2026-03-26 18:00:11 +0100

    ui: remove ActionBar to enable full-screen layout

--- app/src/main/java/cat/teknos/uvicapp/activity/MainActivity.kt
+++ app/src/main/java/cat/teknos/uvicapp/activity/MainActivity.kt
@@ -1,10 +1,7 @@
 package cat.teknos.uvicapp.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import cat.teknos.uvicapp.R
 import cat.teknos.uvicapp.fragment.CardFragment
